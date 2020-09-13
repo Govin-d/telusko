3e55493b335ddc57e6b300662d9066bd23319266
Aravind Venugopal <venugopal.aravind94@gmail.com>
2020-09-13 16:53:14 -0700

    Changed root app to calc

--- calc/urls.py
+++ calc/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 import os
 print(os.getcwd())
-from ..calc import views
+from . import views
 
 urlpatterns = [
     path('',views.home, name='home')
--- telusko/settings.py
+++ telusko/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'telusko.urls'
+ROOT_URLCONF = 'calc.urls'
 
 TEMPLATES = [
     {
